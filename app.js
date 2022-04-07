7f56c2fe7e90a273853ffb79a717e976651ed203
KodoDushimimana <72461740+KodoDushimimana@users.noreply.github.com>
2022-04-07 21:51:23 +0200

    This is the backend that pulls data from stock market and update the airtable with the last price

---
+++ app.js
@@ -0,0 +1,330 @@
+const express = require('express')
+const fetch = require('node-fetch')
+const Airtable = require('airtable')
+const dayjs = require('dayjs');
+const app = express()
+app.use(express.static('public'))
+app.use(express.json())
+require('dotenv').config();
+const port = process.env.PORT || 3000
+
+const stock_API = process.env.STOCK_API_KEY;
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appGWfv8R1VHPYYOA');
+ const yesterday = dayjs().add(-1, 'day').format('YYYY-MM-DD', 'America/New_York');
+
+
+const xom_url = `https://api.polygon.io/v1/open-close/XOM/${yesterday}?adjusted=true&apiKey=${stock_API}`;
+const lng_url = `https://api.polygon.io/v1/open-close/LNG/${yesterday}?adjusted=true&apiKey=${stock_API}`;
+const rio_url = `https://api.polygon.io/v1/open-close/RIO/${yesterday}?adjusted=true&apiKey=${stock_API}`;
+const ceg_url = `https://api.polygon.io/v1/open-close/CEG/${yesterday}?adjusted=true&apiKey=${stock_API}`;
+const bg_url = `https://api.polygon.io/v1/open-close/BG/${yesterday}?adjusted=true&apiKey=${stock_API}`;
+const shlx_url = `https://api.polygon.io/v1/open-close/SHLX/${yesterday}?adjusted=true&apiKey=${stock_API}`;
+const pypl_url = `https://api.polygon.io/v1/open-close/PYPL/${yesterday}?adjusted=true&apiKey=${stock_API}`;
+const rivn_url = `https://api.polygon.io/v1/open-close/RIVN/${yesterday}?adjusted=true&apiKey=${stock_API}`;
+
+app.get('/xom', async(req, res)=>{   
+      const xomData = await fetch(xom_url);
+      const responseData = await xomData.json();
+      const high = responseData.high
+      const fromDate = responseData.from
+      const close = responseData.close
+      const symbol = responseData.symbol
+
+      base('Stock Market').update('recAyaHaz16yYuOrq',
+				{
+					'Symbol': symbol,
+					' Current Price per Share ': close,
+					'Date of Last Price': fromDate,
+				},
+				function (err, record) {
+					if (err) {
+						console.error(err);
+						return;
+					}
+					console.log(record.get('Symbol'));
+				},
+			);
+
+     res.json({
+      high,
+      fromDate,
+      close,
+      symbol
+     })    
+})
+
+//second xom stocks
+app.get('/xom/second', async (req, res) => {
+	const xomData = await fetch(xom_url);
+	const responseData = await xomData.json();
+	const high = responseData.high;
+	const fromDate = responseData.from;
+	const close = responseData.close;
+	const symbol = responseData.symbol;
+
+	base('Stock Market').update(
+		'recyBSK9Q5BDJdQAz',
+		{
+			'Symbol': symbol,
+			' Current Price per Share ': close,
+			'Date of Last Price': fromDate,
+		},
+		function (err, record) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.log(record.get('Symbol'));
+		},
+	);
+
+	res.json({
+		high,
+		fromDate,
+		close,
+		symbol,
+	});
+});
+
+//LNG stocks starts here
+
+app.get('/lng', async (req, res) => {
+	const lngData = await fetch(lng_url);
+	const responseData = await lngData.json();
+	const high = responseData.high;
+	const fromDate = responseData.from;
+	const close = responseData.close;
+	const symbol = responseData.symbol;
+
+	base('Stock Market').update(
+		'recdYFUuiq056NcuG',
+		{
+			'Symbol': symbol,
+			' Current Price per Share ': close,
+			'Date of Last Price': fromDate,
+		},
+		function (err, record) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.log(record.get('Symbol'));
+		},
+	);
+
+	res.json({
+		high,
+		fromDate,
+		close,
+		symbol,
+	});
+});
+
+//RIO stocks starts here
+
+app.get('/rio', async (req, res) => {
+	const rioData = await fetch(rio_url);
+	const responseData = await rioData.json();
+	const high = responseData.high;
+	const fromDate = responseData.from;
+	const close = responseData.close;
+	const symbol = responseData.symbol;
+
+	base('Stock Market').update(
+		'recWhrR8NIfH4TbPl',
+		{
+			'Symbol': symbol,
+			' Current Price per Share ': close,
+			'Date of Last Price': fromDate,
+		},
+		function (err, record) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.log(record.get('Symbol'));
+		},
+	);
+
+	res.json({
+		high,
+		fromDate,
+		close,
+		symbol,
+	});
+});
+
+
+//CEG stocks starts here
+
+app.get('/ceg', async (req, res) => {
+	const cegData = await fetch(ceg_url);
+	const responseData = await cegData.json();
+	const high = responseData.high;
+	const fromDate = responseData.from;
+	const close = responseData.close;
+	const symbol = responseData.symbol;
+
+	base('Stock Market').update(
+		'recDZh15KcINdTgs4',
+		{
+			'Symbol': symbol,
+			' Current Price per Share ': close,
+			'Date of Last Price': fromDate,
+		},
+		function (err, record) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.log(record.get('Symbol'));
+		},
+	);
+
+	res.json({
+		high,
+		fromDate,
+		close,
+		symbol,
+	});
+});
+
+//BG starts here
+
+app.get('/bg', async (req, res) => {
+	const bgData = await fetch(bg_url);
+	const responseData = await bgData.json();
+	const high = responseData.high;
+	const fromDate = responseData.from;
+	const close = responseData.close;
+	const symbol = responseData.symbol;
+
+	base('Stock Market').update(
+		'rechKj6XDGrtT9A3M',
+		{
+			'Symbol': symbol,
+			' Current Price per Share ': close,
+			'Date of Last Price': fromDate,
+		},
+		function (err, record) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.log(record.get('Symbol'));
+		},
+	);
+
+	res.json({
+		high,
+		fromDate,
+		close,
+		symbol,
+	});
+});
+
+//SHLX stocks starts here
+
+app.get('/shlx', async (req, res) => {
+	const shlxData = await fetch(shlx_url);
+	const responseData = await shlxData.json();
+
+	const high = responseData.high;
+	const fromDate = responseData.from;
+	const close = responseData.close;
+	const symbol = responseData.symbol;
+
+	base('Stock Market').update(
+		'rec4yPFzLUuJosIUa',
+		{
+			'Symbol': symbol,
+			' Current Price per Share ': close,
+			'Date of Last Price': fromDate,
+		},
+		function (err, record) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.log(record.get('Symbol'));
+		},
+	);
+
+	res.json({
+		high,
+		fromDate,
+		close,
+		symbol,
+	});
+});
+
+//PYPL stocks starts here
+
+app.get('/pypl', async (req, res) => {
+	const pyplData = await fetch(pypl_url);
+	const responseData = await pyplData.json();
+	const high = responseData.high;
+	const fromDate = responseData.from;
+	const close = responseData.close;
+	const symbol = responseData.symbol;
+
+	base('Stock Market').update(
+		'recBAJvMahczZeMwR',
+		{
+			'Symbol': symbol,
+			' Current Price per Share ': close,
+			'Date of Last Price': fromDate,
+		},
+		function (err, record) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.log(record.get('Symbol'));
+		},
+	);
+
+	res.json({
+		high,
+		fromDate,
+		close,
+		symbol,
+	});
+});
+
+//RIVN Stocks Starts here
+
+app.get('/rivn', async (req, res) => {
+	const rivnData = await fetch(rivn_url);
+	const responseData = await rivnData.json();
+	const high = responseData.high;
+	const fromDate = responseData.from;
+	const close = responseData.close;
+	const symbol = responseData.symbol;
+
+	base('Stock Market').update(
+		'rec1IVhd36ZxTGp3O',
+		{
+			'Symbol': symbol,
+			' Current Price per Share ': close,
+			'Date of Last Price': fromDate,
+		},
+		function (err, record) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.log(record.get('Symbol'));
+		},
+	);
+
+	res.json({
+		high,
+		fromDate,
+		close,
+		symbol,
+	});
+});
+
+
+app.listen(port, ()=>console.log(`Server started on port ${port}`))
