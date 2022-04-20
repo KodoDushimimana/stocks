d809102a50cb31b4607c9569c55f6030803d413c
KodoDushimimana <72461740+KodoDushimimana@users.noreply.github.com>
2022-04-20 18:26:31 +0200

    This is a working code

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const fetch = require('node-fetch')
 const Airtable = require('airtable')
 const dayjs = require('dayjs');
+const res = require('express/lib/response');
 const app = express()
 app.use(express.static('public'))
 app.use(express.json())
@@ -11,7 +12,7 @@ const port = process.env.PORT || 3000
 const stock_API = process.env.STOCK_API_KEY;
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appGWfv8R1VHPYYOA');
  const yesterday = dayjs().add(-1, 'day').format('YYYY-MM-DD', 'America/New_York');
-
+// const yesterday = '2022-04-14'
 		
 // const sunday = dayjs().day(0);
 // const monday = dayjs().day(1);
@@ -27,7 +28,7 @@ const xom_url = `https://api.polygon.io/v1/open-close/XOM/${yesterday}?adjusted=
 const lng_url = `https://api.polygon.io/v1/open-close/LNG/${yesterday}?adjusted=true&apiKey=${stock_API}`;
 const rio_url = `https://api.polygon.io/v1/open-close/RIO/${yesterday}?adjusted=true&apiKey=${stock_API}`;
 const ceg_url = `https://api.polygon.io/v1/open-close/CEG/${yesterday}?adjusted=true&apiKey=${stock_API}`;
-const bg_url = `https://api.polygon.io/v1/open-close/BG/${yesterday}?adjusted=true&apiKey=${stock_API}`;
+const bg_url  =  `https://api.polygon.io/v1/open-close/BG/${yesterday}?adjusted=true&apiKey=${stock_API}`;
 const shlx_url = `https://api.polygon.io/v1/open-close/SHLX/${yesterday}?adjusted=true&apiKey=${stock_API}`;
 const pypl_url = `https://api.polygon.io/v1/open-close/PYPL/${yesterday}?adjusted=true&apiKey=${stock_API}`;
 const rivn_url = `https://api.polygon.io/v1/open-close/RIVN/${yesterday}?adjusted=true&apiKey=${stock_API}`;
@@ -35,14 +36,27 @@ const rivn_url = `https://api.polygon.io/v1/open-close/RIVN/${yesterday}?adjuste
 app.get('/xom', async(req, res)=>{   	
 	
 		const xomData = await fetch(xom_url);
-    const responseData = await xomData.json();
+    const responseData = await xomData.json();		
+		const status = responseData.status
+		const high = responseData.high;
+		const fromDate = responseData.from;
+		const close = responseData.close;
+		const symbol = responseData.symbol;
 		
-		
-    const high = responseData.high
-    const fromDate = responseData.from
-    const close = responseData.close
-    const symbol = responseData.symbol
-		
+		if(status === 'NOT_FOUND'){
+			res.json({
+				status
+			})
+			return
+		}else{
+		res.json({
+			high,
+			fromDate,
+			close,
+			symbol
+		});    
+		}
+    
     base('Stock Market').update('recAyaHaz16yYuOrq',
 				{
 					'Symbol': symbol,
@@ -58,12 +72,7 @@ app.get('/xom', async(req, res)=>{
 				},
 			);
 
-     res.json({
-      high,
-      fromDate,
-      close,
-      symbol
-     })    
+     
 
 			 
 })
@@ -72,11 +81,27 @@ app.get('/xom', async(req, res)=>{
 app.get('/xom/second', async (req, res) => {
 	const xomData = await fetch(xom_url);
 	const responseData = await xomData.json();
+	
+	const status = responseData.status
 	const high = responseData.high;
 	const fromDate = responseData.from;
 	const close = responseData.close;
 	const symbol = responseData.symbol;
 
+
+	if(status ==='NOT_FOUND'){
+		res.json({ status	})
+		return
+	}else{
+		res.json({
+			high,
+			fromDate,
+			close,
+			symbol,
+		});
+
+	}
+	
 	base('Stock Market').update(
 		'recyBSK9Q5BDJdQAz',
 		{
@@ -93,12 +118,7 @@ app.get('/xom/second', async (req, res) => {
 		},
 	);
 
-	res.json({
-		high,
-		fromDate,
-		close,
-		symbol,
-	});
+	
 });
 
 //LNG stocks starts here
@@ -106,10 +126,25 @@ app.get('/xom/second', async (req, res) => {
 app.get('/lng', async (req, res) => {
 	const lngData = await fetch(lng_url);
 	const responseData = await lngData.json();
+
+	const status = responseData.status
 	const high = responseData.high;
 	const fromDate = responseData.from;
 	const close = responseData.close;
 	const symbol = responseData.symbol;
+
+	if(status === "NOT_FOUND"){
+		res.json({status})
+		return
+	}else{
+		res.json({
+			high,
+			fromDate,
+			close,
+			symbol,
+		});
+
+	}
 
 	base('Stock Market').update(
 		'recdYFUuiq056NcuG',
@@ -127,12 +162,7 @@ app.get('/lng', async (req, res) => {
 		},
 	);
 
-	res.json({
-		high,
-		fromDate,
-		close,
-		symbol,
-	});
+	
 });
 
 //RIO stocks starts here
@@ -140,10 +170,26 @@ app.get('/lng', async (req, res) => {
 app.get('/rio', async (req, res) => {
 	const rioData = await fetch(rio_url);
 	const responseData = await rioData.json();
+
+	const status = responseData.status;
 	const high = responseData.high;
 	const fromDate = responseData.from;
 	const close = responseData.close;
 	const symbol = responseData.symbol;
+
+	if (status === 'NOT_FOUND') {
+		res.json({
+			status,
+		});
+		return;
+	} else {
+		res.json({
+			high,
+			fromDate,
+			close,
+			symbol,
+		});
+	}
 
 	base('Stock Market').update(
 		'recWhrR8NIfH4TbPl',
@@ -161,12 +207,7 @@ app.get('/rio', async (req, res) => {
 		},
 	);
 
-	res.json({
-		high,
-		fromDate,
-		close,
-		symbol,
-	});
+	
 });
 
 
@@ -175,10 +216,25 @@ app.get('/rio', async (req, res) => {
 app.get('/ceg', async (req, res) => {
 	const cegData = await fetch(ceg_url);
 	const responseData = await cegData.json();
+	const status = responseData.status;	
 	const high = responseData.high;
 	const fromDate = responseData.from;
 	const close = responseData.close;
 	const symbol = responseData.symbol;
+
+	if (status === 'NOT_FOUND') {
+		res.json({
+			status,
+		});
+		return;
+	} else {
+		res.json({
+			high,
+			fromDate,
+			close,
+			symbol,
+		});
+	}
 
 	base('Stock Market').update(
 		'recDZh15KcINdTgs4',
@@ -196,12 +252,7 @@ app.get('/ceg', async (req, res) => {
 		},
 	);
 
-	res.json({
-		high,
-		fromDate,
-		close,
-		symbol,
-	});
+	
 });
 
 //BG starts here
@@ -210,11 +261,25 @@ app.get('/bg', async (req, res) => {
 	const bgData = await fetch(bg_url);
 	const responseData = await bgData.json();
 
-
+	const status = responseData.status;
 	const high = responseData.high;
 	const fromDate = responseData.from;
 	const close = responseData.close;
 	const symbol = responseData.symbol;
+
+	if (status === 'NOT_FOUND') {
+		res.json({
+			status,
+		});
+		return;
+	} else {
+		res.json({
+			high,
+			fromDate,
+			close,
+			symbol,
+		});
+	}
 
 	base('Stock Market').update(
 		'rechKj6XDGrtT9A3M',
@@ -232,12 +297,7 @@ app.get('/bg', async (req, res) => {
 		},
 	);
 
-	res.json({
-		high,
-		fromDate,
-		close,
-		symbol,
-	});
+	
 });
 
 //SHLX stocks starts here
@@ -246,12 +306,25 @@ app.get('/shlx', async (req, res) => {
 	const shlxData = await fetch(shlx_url);
 	const responseData = await shlxData.json();
 
-
+	const status = responseData.status;
 	const high = responseData.high;
 	const fromDate = responseData.from;
 	const close = responseData.close;
 	const symbol = responseData.symbol;
-
+  
+	if (status === 'NOT_FOUND') {
+		res.json({
+			status,
+		});
+		return;
+	} else {
+		res.json({
+			high,
+			fromDate,
+			close,
+			symbol,
+		});
+	}
 	base('Stock Market').update(
 		'rec4yPFzLUuJosIUa',
 		{
@@ -268,12 +341,7 @@ app.get('/shlx', async (req, res) => {
 		},
 	);
 
-	res.json({
-		high,
-		fromDate,
-		close,
-		symbol,
-	});
+	
 });
 
 //PYPL stocks starts here
@@ -282,11 +350,25 @@ app.get('/pypl', async (req, res) => {
 	const pyplData = await fetch(pypl_url);
 	const responseData = await pyplData.json();
 
+	const status = responseData.status;
 	const high = responseData.high;
 	const fromDate = responseData.from;
 	const close = responseData.close;
 	const symbol = responseData.symbol;
 
+	if (status === 'NOT_FOUND') {
+		res.json({
+			status,
+		});
+		return;
+	} else {
+		res.json({
+			high,
+			fromDate,
+			close,
+			symbol,
+		});
+	}
 	base('Stock Market').update(
 		'recBAJvMahczZeMwR',
 		{
@@ -303,12 +385,7 @@ app.get('/pypl', async (req, res) => {
 		},
 	);
 
-	res.json({
-		high,
-		fromDate,
-		close,
-		symbol,
-	});
+	
 });
 
 //RIVN Stocks Starts here
@@ -316,12 +393,26 @@ app.get('/pypl', async (req, res) => {
 app.get('/rivn', async (req, res) => {
 	const rivnData = await fetch(rivn_url);
 	const responseData = await rivnData.json();
-	
+
+	const status = responseData.status;		
 	const high = responseData.high;
 	const fromDate = responseData.from;
 	const close = responseData.close;
 	const symbol = responseData.symbol;
 
+	if (status === 'NOT_FOUND') {
+		res.json({
+			status,
+		});
+		return;
+	} else {
+		res.json({
+			high,
+			fromDate,
+			close,
+			symbol,
+		});
+	}
 	base('Stock Market').update(
 		'rec1IVhd36ZxTGp3O',
 		{
@@ -337,14 +428,29 @@ app.get('/rivn', async (req, res) => {
 			//console.log(record.get('Symbol'));
 		},
 	);
-
-	res.json({
-		high,
-		fromDate,
-		close,
-		symbol,
-	});
 });
+
+//getting records from Airtable
+
+app.get('/records', (req, res) =>{
+
+const retrievedData = () => {base('Post Data').find('recrEZcXuLgqXJsrx', function (err, record) {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		console.log('Retrieved', record);
+		
+	});
+
+}
+
+res.json(retrievedData())
+
+
+
+})
+	
 
 
 app.listen(port, ()=>console.log(`Server started on port ${port}`))
